4f3b9d012f24d75a7dd7bd80dea3386b3ca17792
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2020-08-10 14:05:42 +0200

    bossbaby: added script call to wrapper

--- wgpp_wrapper.sh
+++ wgpp_wrapper.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 $HOME/.wg++/run.sh
-$HOME/postproc-scripts/peppa/remap_peppa_episodes.pl $HOME/public/epg.xml > $HOME/public/epg-post.xml
+$HOME/postproc-scripts/peppa/remap_peppa_episodes.pl $HOME/public/epg.xml > $HOME/public/epg-post1.xml
+$HOME/postproc-scripts/bossbaby/remap_episodes.pl $HOME/public/epg-post1.xml > $HOME/public/epg-post.xml
